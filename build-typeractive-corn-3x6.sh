969013fe5f273e49922bfc91637b8fb2948a38ab
btilford <248725+btilford@users.noreply.github.com>
2025-07-13 16:34:40 -0600

    adding a reset settings firmware

---
+++ build-typeractive-corn-3x6.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env zsh
+
+set -e
+echo "Building ZMK for Typeractive Corn 3x6"
+west build \
+    -p \
+    -s $HOME/Projects/public/corne-wireless-view-zmk-config/zmk/app/ \
+    -b nice_nano_v2 \
+    -- -DSHIELD=corne_left \
+    -DZMK_CONFIG=$HOME/Projects/public/corne-wireless-view-zmk-config/config/ \
+    -DZMK_EXTRA_MODULES=/home/btilford/Projects/public/zmk-leader-key
+
+
+echo "Left side build completed."
+
+
+# west build -s app/ \
+#     -d /tmp/zmk-corn-right \
+#     -b nice_nano_v2 \
+#     -- -DSHIELD=corne_right \
+#  -DZMK_CONFIG=$HOME/Projects/public/corne-wireless-view-zmk-config \
+#     -DZMK_EXTRA_MODULES=/home/btilford/Projects/public/zmk-leader-key
+#
+    # -DZMK_CONFIG=$HOME/Projects/public/corne-wireless-view-zmk-config/config/ \
+
